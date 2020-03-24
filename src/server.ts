5a587dffe44fb17066d3e4350d2ef5a222cd5070
Waruka Shotta <alkashotta@gmail.com>
2020-03-24 21:50:21 +0800

    make it better

--- src/server.ts
+++ src/server.ts
@@ -63,6 +63,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen().then((url: any) => {
+server.listen({ port: process.env.PORT || 4000 }).then((url: any) => {
   console.log(`🚀  Server ready at ${url.url}`);
 });
