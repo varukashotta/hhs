5e8989107806650b7b813f2341313bc0593d1ef6
Waruka Shotta <alkashotta@gmail.com>
2020-03-31 12:20:56 +0800

    Update Version

--- src/datasources/twitter.ts
+++ src/datasources/twitter.ts
@@ -1,25 +1,20 @@
-import { RESTDataSource }from "apollo-datasource-rest";
+import { RESTDataSource, RequestOptions } from "apollo-datasource-rest";
 import dotenv from "dotenv";
-import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
 class TwitterAPI extends RESTDataSource {
-  constructor() {
-    super();
-    this.baseURL =
-      `https://api.twitter.com/1.1/search/tweets.json?q=%23covid19&result_type=popular`;
-  }
+  baseURL = `https://api.twitter.com/1.1/search/tweets.json?q=%23covid19&result_type=popular`;
 
-  willSendRequest(request: { headers: { set: (arg0: string, arg1: any) => void; }; }) {
-    request.headers.set('Authorization', 'Bearer AAAAAAAAAAAAAAAAAAAAAJPE0AAAAAAAl2%2BUgMIsU%2BjaJsD3RkkMuUdy%2FHg%3DV4aoK5xegk11ey36gScWEd4YYEQoV3dsDJfpYVLgNHV6q5HboF');
+  willSendRequest(request: RequestOptions) {
+    request.headers.set(
+      "Authorization",
+      `Bearer ${process.env.TWITTER_BEARER_TOKEN}`
+    );
   }
 
   postsReducer(post: any) {
-
-    const {id, user, text, created_at} = post;
-
-    console.log(user);
+    const { id, user, text, created_at } = post;
 
     return {
       id,
@@ -33,8 +28,6 @@ class TwitterAPI extends RESTDataSource {
     const response = await this.get("/");
 
     const responsePosts = response.statuses;
-
-    console.log(response);
 
     return Array.isArray(responsePosts)
       ? responsePosts.map((posts: any) => this.postsReducer(posts))
