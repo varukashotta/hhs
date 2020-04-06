45bd50c6f0a6e8f6fa3326f76523bbcf60363466
Waruka Shotta <alkashotta@gmail.com>
2020-04-06 14:05:06 +0800

    Update Version

--- src/queue/processes/search.ts
+++ src/queue/processes/search.ts
@@ -1,11 +1,8 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 
-import createQueue from '../index';
-import { logger } from '../../log/index';
-import _ from 'lodash';
-
-
+import createQueue from "../index";
+import { logger } from "../../log/index";
 
 // const data = {
 //   email: 'foo@bar.com',
@@ -16,54 +13,30 @@ import _ from 'lodash';
 //   attempts: 3,
 // };
 
-
-
-
-const processSearch = async (data:any, options = {}, callback: ((arg0: any, arg1: any) => any)) => {
-  const searchQueue = createQueue('search');
-
-  var after100 = _.after(100, function() {
-    searchQueue.close().then(function() {
-      console.log('done');
-    });
-  });
+const processSearch = async (
+  data: any,
+  options = {},
+  callback: (arg0: any, arg1: any) => any
+) => {
+  const searchQueue = createQueue("search");
 
   try {
     await searchQueue.add(data, options);
   } catch (e) {
-    console.log(e);
+    logger.error(e);
   }
 
   try {
     await searchQueue.process(async (job, done) => {
-     let you =  await callback(job.data, job);
-
-
+      await callback(job.data, job);
 
       searchQueue.close();
 
-      // return you;
-
-
-      // let test = await job.isCompleted();
-
-      // console.log(test);
-      // searchQueue.on('completed', after100);
-      // searchQueue.clean(15000);
-
-      // searchQueue.on('progress', after100)
-
       done(null, "test");
-
-
     });
   } catch (e) {
     return e;
   }
-
-
-
-  searchQueue.on('completed',  () => console.log('done'));
 };
 
 export default processSearch;
